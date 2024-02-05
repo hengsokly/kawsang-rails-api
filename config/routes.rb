f6d96c39f28a27027a73db9a520c3f30850c370c
Sokly <hengsokly23@gmail.com>
2024-02-05 10:48:27 +0700

    Update description in routes for login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,32 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # ------**Login**--------
+  # POST http://localhost:3000/oauth/token?grant_type=password&email=USER_EMAIL&password=USER_PASSWORD&client_id=CLIENT_ID&client_secret=CLIENT_SECRET
+  # Replace USER_EMAIL, USER_PASSWORD, CLIENT_ID, CLIENT_SECRET
+  # Note: CLIENT_ID, CLIENT_SECRET are from your doorkeeper application
+  # Response:
+  #   {
+  #     "access_token": "n5GeVhicBSxIg1tJ8AEfqJ4I3lpS8H8NGtg6bnCvjUU",
+  #     "token_type": "Bearer",
+  #     "expires_in": 7200,
+  #     "refresh_token": "9DYVS0sOeec5ihhGeDRhgTBlhBDrbKHBCFURamqiaTM",
+  #     "created_at": 1707100357
+  #   }
+  # Readmore: https://rubyyagi.com/rails-api-authentication-devise-doorkeeper/
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  # get "up" => "rails/health#show", as: :rails_health_check
+  # Logout: To log out a user, we can revoke the access token, and to revoke an access token, we need to send a HTTP POST request to /oauth/revoke, with token, client_id and client_secret attributes.
+  #   POST http://localhost:3000/oauth/revoke?token=n5GeVhicBSxIg1tJ8AEfqJ4I3lpS8H8NGtg6bnCvjUU&client_id=VvWJc2jderqHLqX9dFVWJtkU6jjKUvs3shB4hut33Gc&client_secret=LmO8FDNh8NTMI-iMR39b2ub88a2u7k3HIM6YShsRXYc
 
+  # Refresh token: To retrieve a new access token when the current access token is (almost) expired, we can send a HTTP POST to /oauth/token
+  #   POST http://localhost:3000/oauth/token?grant_type=refresh_token&refresh_token=AKZ_uZV6MzgZ2GVGxzBnXVewl-o0IpCBFggqox7i2FM&client_id=VvWJc2jderqHLqX9dFVWJtkU6jjKUvs3shB4hut33Gc&client_secret=LmO8FDNh8NTMI-iMR39b2ub88a2u7k3HIM6YShsRXYc
   use_doorkeeper do
     skip_controllers :authorizations, :applications, :authorized_applications
   end
+  # ------****--------
 
-  # Defines the root path route ("/")
-  # root "posts#index"
-
+  # Get resource
+  #   GET http://localhost:3000/v1/users
+  #   Header: Authorization: Bearer ACCESS_TOKEN
+  #   Replace ACCESS_TOKEN with the token you get from above
   namespace :v1 do
     resources :users, only: %i[index]
   end
